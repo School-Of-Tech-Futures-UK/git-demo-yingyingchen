af8e014fc5f7e33e468610ab0f7362e3b6d9db91
YingyingEChen <yingying.e.chen@accenture.com>
2022-01-12 17:14:04 +0000

    Finish User Story 3

--- Connect4/index.js
+++ Connect4/index.js
@@ -11,25 +11,25 @@ const map = {
 }
 
 // create board
-for (i=0; i<rowNum; i++) {
+for (i = 0; i < rowNum; i++) {
     const colArr = []
-    for (j = 0; j<colNum; j++) {
+    for (j = 0; j < colNum; j++) {
         colArr.push(null)
     }
     board.push(colArr.map(x => x))
 }
 
 //_board
-for (i=0; i<rowNum; i++) {
+for (i = 0; i < rowNum; i++) {
     const _colArr = []
-    for (j = 0; j<colNum; j++) {
+    for (j = 0; j < colNum; j++) {
         _colArr.push(0)
     }
     _board.push(_colArr.map(x => x))
 }
 
 function getAvailableColumn(rowSelected) {
-    for (let j=colNum-1; j>=0; j--) {
+    for (let j = colNum - 1; j >= 0; j--) {
         if (board[rowSelected][j] === null) {
             return j
         }
@@ -52,7 +52,7 @@ function takeTurn(rowSelected) {
         }
     }
     console.log(`row selected: ${rowSelected} column selected: ${columnSelected}`)
-    console.log(board)
+    console.log(_board)
 }
 
 // clear the board
@@ -78,13 +78,6 @@ function drawBoard(board) {
     }
 }
 
-// click the column, play the game, record the game state, and check for winner
-function positionClick(ev) {
-    const id = ev.target.id
-    const rowSelected = id[4]
-    takeTurn(rowSelected)
-    drawBoard(board)
-}
 
 // reset game
 function resetGame() {
@@ -96,6 +89,120 @@ function resetGame() {
     }
     turn = "red"
     document.querySelectorAll(".column").forEach((cube) => cube.style.background = "white")
+    const winnerName = document.getElementById("winner-name");
+    winnerName.innerText = "";
+    const winnerDisplay = document.getElementById("winner-display");
+    winnerDisplay.style.display = "None";
     console.log("resetGame was called");
     console.log(board)
+}
+
+// check for winner
+function checkWinnerInArray(arr) {
+    for (j = 0; j < arr.length - 3; j++) {
+        sum = arr.slice(j, j + 4).reduce((prev, curr) => prev + curr, 0)
+        if (sum === 4) {
+            turn = null
+            return "red"
+        } else if (sum === -4) {
+            turn = null
+            return "yellow"
+        }
+    }
+}
+
+function checkWinner() {
+    let i, j, winner
+
+    // check row
+    for (i = 0; i < rowNum; i++) {
+        const rowChecking = _board[i]
+        winner = checkWinnerInArray(rowChecking)
+        if (winner) {
+            return winner
+        }
+    }
+
+    // check col
+    for (j = 0; j < colNum; j++) {
+        const columnChecking = []
+        for (i = 0; i < rowNum; i++) {
+            columnChecking.push(_board[i][j])
+        }
+        winner = checkWinnerInArray(columnChecking)
+        if (winner) {
+            return winner
+        }
+    }
+
+    // check top left to bottom right
+    const maxLength = Math.max(rowNum, colNum);
+    let diagChecking
+    for (let k = 0; k <= 2 * (maxLength - 1); k++) {
+        diagChecking = [];
+        for (let y = rowNum - 1; y >= 0; y--) {
+            let x = k - y;
+            if (x >= 0 && x < colNum) {
+                diagChecking.push(_board[y][x]);
+            }
+        }
+        if (diagChecking.length > 3) {
+            winner = checkWinnerInArray(diagChecking)
+            if (winner) {
+                return winner
+            }
+        }
+    }
+
+    // check bottom left to top right
+    for (var k = 0; k <= 2 * (maxLength - 1); k++) {
+        diagChecking = [];
+        for (var y = rowNum - 1; y >= 0; y--) {
+            var x = k - (rowNum - y);
+            if (x >= 0 && x < colNum) {
+                diagChecking.push(_board[y][x]);
+            }
+        }
+        if (diagChecking.length > 3) {
+            winner = checkWinnerInArray(diagChecking)
+            if (winner) {
+                return winner
+            }
+        }
+    }
+
+    // check if game is finished
+    let gameFinish = true
+    for (i = 0; i < rowNum; i++) {
+        for (j = 0; j < colNum; j++) {
+            if (_board[i][j] === 0) {
+                gameFinish = false
+            }
+        }
+    }
+    if (gameFinish === true) {
+        return "nobody"
+    }
+
+    console.log("checkWinner was called");
+    return null;
+}
+
+
+// click the column, play the game, record the game state, and check for winner
+function positionClick(ev) {
+    const id = ev.target.id
+    const rowSelected = id[4]
+    takeTurn(rowSelected)
+    drawBoard(board)
+    const winner = checkWinner()
+    if (winner) {
+        if (typeof winner !== "string" || !["red", "yellow", "nobody"].includes(winner)) {
+            throw "Expecting 'checkWinner' to return null or one of the strings 'red', 'yellow' or 'nobody'. Actually received: " + winner;
+        }
+        const winnerName = document.getElementById("winner-name");
+        winnerName.innerText = winner;
+        const winnerDisplay = document.getElementById("winner-display");
+        winnerDisplay.style.display = "block";
+    }
 }
