885ca6e0a7155e301510dc2a589c7b50ec11fad6
YingyingEChen <yingying.e.chen@accenture.com>
2022-01-14 15:18:18 +0000

    create state object

--- Connect4/index.js
+++ Connect4/index.js
@@ -1,41 +1,4 @@
-const rowNum = 7
-const colNum = 6
-const board = [], _board = []
 
-let i, j
-let turn = "red"
-
-const map = {
-    red: 1,
-    yellow: -1
-}
-
-// create board
-for (i = 0; i < rowNum; i++) {
-    const colArr = []
-    for (j = 0; j < colNum; j++) {
-        colArr.push(null)
-    }
-    board.push(colArr.map(x => x))
-}
-
-//_board
-for (i = 0; i < rowNum; i++) {
-    const _colArr = []
-    for (j = 0; j < colNum; j++) {
-        _colArr.push(0)
-    }
-    _board.push(_colArr.map(x => x))
-}
-
-function getAvailableColumn(rowSelected) {
-    for (let j = colNum - 1; j >= 0; j--) {
-        if (board[rowSelected][j] === null) {
-            return j
-        }
-    }
-    return "full"
-}
 
 // Impure
 // play the game and change the game state
@@ -211,4 +174,21 @@ function positionClick(ev) {
         const winnerDisplay = document.getElementById("winner-display");
         winnerDisplay.style.display = "block";
     }
+}
+
+if (typeof exports === 'object') {
+    console.log("Running in Node")
+    // Node. Does not work with strict CommonJS, but only CommonJS-like 
+    // environments that support module.exports, like Node.
+    module.exports = {
+        takeTurn,
+        checkWinnerInArray,
+        checkWinner,
+        resetGame,
+        clearBoard,
+        drawBoard,
+        positionClick
+    }
+} else {
+    console.log("Running in Browser")
 }
---
+++ Connect4/state_init.js
@@ -0,0 +1,55 @@
+const rowNum = 7
+const colNum = 6
+const board = [], _board = []
+
+let i, j
+
+const map = {
+    red: 1,
+    yellow: -1
+}
+
+// create board
+for (i = 0; i < rowNum; i++) {
+    const colArr = []
+    for (j = 0; j < colNum; j++) {
+        colArr.push(null)
+    }
+    board.push(colArr.map(x => x))
+}
+
+//_board
+for (i = 0; i < rowNum; i++) {
+    const _colArr = []
+    for (j = 0; j < colNum; j++) {
+        _colArr.push(0)
+    }
+    _board.push(_colArr.map(x => x))
+}
+
+function getAvailableColumn(rowSelected) {
+    for (let j = colNum - 1; j >= 0; j--) {
+        if (board[rowSelected][j] === null) {
+            return j
+        }
+    }
+    return "full"
+}
+
+const state = {
+    turn: "red",
+    board: [...board],
+    _board: [..._board]
+}
+
+if (typeof exports === 'object') {
+    console.log("Running in Node")
+    // Node. Does not work with strict CommonJS, but only CommonJS-like 
+    // environments that support module.exports, like Node.
+    module.exports = {
+        getAvailableColumn,
+        state
+    }
+} else {
+    console.log("Running in Browser")
+}
